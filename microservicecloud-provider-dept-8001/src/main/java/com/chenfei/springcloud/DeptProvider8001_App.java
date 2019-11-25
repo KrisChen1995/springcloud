7e4f4fc3b071c74eab351251f2f07c09705cbd24
chen <18955928961@163.com>
2019-11-25 16:17:44 +0800

    second commit

--- microservicecloud-provider-dept-8001/src/main/java/com/chenfei/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/chenfei/springcloud/DeptProvider8001_App.java
@@ -2,8 +2,10 @@ package com.chenfei.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient  //本服务启动后会自动注册进eureka中
 public class DeptProvider8001_App{
 
     public static void main(String[] args){
--- microservicecloud-provider-dept-8001/src/main/java/com/chenfei/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/chenfei/springcloud/controller/DeptController.java
@@ -3,14 +3,19 @@ package com.chenfei.springcloud.controller;
 import com.chenfei.springcloud.entities.Dept;
 import com.chenfei.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class DeptController {
+
     @Autowired
     private DeptService deptService ;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST )
     public boolean add(@RequestBody Dept dept){
